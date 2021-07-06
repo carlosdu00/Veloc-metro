445739a2fc635ebfe0dc2f6fe90360d8d75b1236
Carlos Schumacher <carloscreed@live.com>
2021-07-06 15:47:29 -0300

    comentarios e reorganização

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const botaocarro = document.getElementById("botaocarro")
 const botaoclicado = document.getElementsByClassName("botaoclicado")
 
 function escolhaInicial(veiculoEscolhido) {
+// aqui, quando um botao é clicado ele ganha as propriedades css de botaoclicado
     if(veiculoEscolhido == moto){
         botaocarro.className=("botaonormal")
         botaomoto.className=("botaoclicado")
@@ -22,24 +23,65 @@ function escolhaInicial(veiculoEscolhido) {
     VelocidadeAntiga = 1
 }
 
+//função usada para o ponteiro se mover lentamente e não estantaneamente
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-document.addEventListener("keyup", function (event) {//detecta qual tecla esta sendo precionada
+async function moverPonteiro() {
+    // a velocidade antiga deve ser guardada para o ponteiro se orientar para qual direção ele deve seguir
+    velocidadeMostradaPonteiro = VelocidadeAntiga
+    ponteiro = document.getElementById("ponteiro")
+    if (VelocidadeAntiga < veiculo.velocidade) {
+        while (velocidadeMostradaPonteiro < veiculo.velocidade) {
+            await sleep(10);
+            velocidadeMostradaPonteiro += 1
+            //aqui é feita a rotação do ponteiro
+            ponteiro.setAttribute("style", "transform: rotate(" + (velocidadeMostradaPonteiro * 221 / 100 - 7) + "deg)");
+        }
+    } else {
+        while (velocidadeMostradaPonteiro > veiculo.velocidade) {
+            await sleep(10);
+            velocidadeMostradaPonteiro -= 1
+            //aqui é feita a rotação do ponteiro
+            ponteiro.setAttribute("style", "transform: rotate(" + (velocidadeMostradaPonteiro * 221 / 100 - 7) + "deg)");
+        }
+    }
+    {// aqui fazemos o ponteiro ter um movimento de como se estivesse tremento (não é realista mas é legal)
+    while(veiculo.velocidade!=0)
+        await sleep(50);
+        // fazemos isso alterando um pouquinho a rotação da imagem
+        ponteiro.setAttribute("style", "transform: rotate(" + (velocidadeMostradaPonteiro * 221 / 100 - 6.5) + "deg)");
+        await sleep(50);
+        ponteiro.setAttribute("style", "transform: rotate(" + (velocidadeMostradaPonteiro * 221 / 100 - 7.5) + "deg)");
+    }
+}   
+
+function salvarVelocidade() {
+    VelocidadeAntiga = veiculo.velocidade //Importante para o ponteiro fazer a animaçao para o lado certo
+}
+
+//detecta qual tecla esta sendo pressionada e chama a funçao correspondante
+document.addEventListener("keyup", function (event) {
     console.log(event.key)
     switch (event.key) {
         case "ArrowUp":
-            salvarVelocidade(); veiculo.acelerar(); moverPonteiro();
+            salvarVelocidade();
+            veiculo.acelerar();
+            moverPonteiro();
             break;
         case "ArrowDown":
-            salvarVelocidade(); veiculo.freiar(); moverPonteiro();
+            salvarVelocidade();
+            veiculo.freiar();
+            moverPonteiro();
             break;
         case "a":
-            veiculo.subirMarcha(); atualizarMostradorMarcha()
+            veiculo.subirMarcha();
+            atualizarMostradorMarcha()
             break;
         case "z":
-            veiculo.descerMarcha(); atualizarMostradorMarcha()
+            veiculo.descerMarcha();
+            atualizarMostradorMarcha()
             break;
         default:
             break;
@@ -51,16 +93,20 @@ const moto = {
     marcha: 0,
     acelerar: function () {
         verificarVelocidade("acelerar");
-        if (moto.velocidade > 100) {
-            moto.velocidade = 100
+        //caso a velocidade tenha execido os 100, corrigimos isso aqui
+        if (this.velocidade > 100) {
+            this.velocidade = 100
         }
     },
     freiar: function () {
-        (this.velocidade >= 5) ? this.velocidade -= 5 : this.velocidade = 0
+        //importante para que a velocidade acabe ficando negativa!
+        (this.velocidade >= 5) ? this.velocidade -= 5 : this.velocidade = 0 
     },
+    //marcha não pode passar de 5
     subirMarcha: function () {
         if (this.marcha < 5) { this.marcha += 1 }
     },
+
     descerMarcha: function () {
         if (this.marcha > 0 && verificarVelocidade("descer")) {
             this.marcha -= 1
@@ -72,19 +118,24 @@ const carro = {
     velocidade: 0,
     marcha: 0,
     acelerar: function () {
+        //caso a velocidade tenha execido os 100, corrigimos isso aqui
         verificarVelocidade("acelerar");
-        if (carro.velocidade > 100) {
-            carro.velocidade = 100
+        if (this.velocidade > 100) {
+            this.velocidade = 100
         }
     },
     freiar: function () {
+        //importante para que a velocidade acabe ficando negativa!
         (this.velocidade >= 5) ? this.velocidade -= 5 : this.velocidade = 0
     },
     subirMarcha: function () {
+        //marcha não pode passar de 5
         if (this.marcha < 5) { this.marcha += 1 }
     },
+
     descerMarcha: function () {
-        if (this.marcha == 0) {
+        // se o carro estiver no neutro e parado, pode engatar a ré
+        if (this.marcha == 0 && this.velocidade == 0) {
             this.marcha -= 1
         }
         else if (this.marcha > -1 && verificarVelocidade("descer")) {
@@ -146,31 +197,7 @@ function verificarVelocidade(acao) {
     }
 }
 
-async function moverPonteiro() {
-    velocidadeMostradaPonteiro = VelocidadeAntiga
-    ponteiro = document.getElementById("ponteiro")
-    if (VelocidadeAntiga < veiculo.velocidade) {
-        while (velocidadeMostradaPonteiro < veiculo.velocidade) {
-            await sleep(10);
-            velocidadeMostradaPonteiro += 1
-            ponteiro.setAttribute("style", "transform: rotate(" + (velocidadeMostradaPonteiro * 221 / 100 - 7) + "deg)");
-        }
-    } else {
-        while (velocidadeMostradaPonteiro > veiculo.velocidade) {
-            await sleep(10);
-            velocidadeMostradaPonteiro -= 1
-            ponteiro.setAttribute("style", "transform: rotate(" + (velocidadeMostradaPonteiro * 221 / 100 - 7) + "deg)");
-        }
-    }
-    while(veiculo.velocidade!=0){
-        await sleep(50);
-        ponteiro.setAttribute("style", "transform: rotate(" + (velocidadeMostradaPonteiro * 221 / 100 - 6.5) + "deg)");
-        await sleep(50);
-        ponteiro.setAttribute("style", "transform: rotate(" + (velocidadeMostradaPonteiro * 221 / 100 - 7.5) + "deg)");
-    }
-    
-}   
-
+// mostrador q efica no meio do velocimeto que indica a marcha atual
 const mostradorMarcha = document.getElementById("mostradorMarcha")
 function atualizarMostradorMarcha() {
     if (veiculo.marcha == 0) {
@@ -185,6 +212,4 @@ function atualizarMostradorMarcha() {
 
 }
 
-function salvarVelocidade() {
-    VelocidadeAntiga = veiculo.velocidade //Importante para o ponteiro fazer a animaçao para o lado certo
-}
+
